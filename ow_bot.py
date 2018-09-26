199936aad878d4200b0437ad8ecdd08a00738bf2
rskrz <ryanskrzypek@gmail.com>
2018-09-26 12:35:57 -0500

    Add files via upload

---
+++ ow_bot.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+import random
+from ranks import *
+
+'''description = An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='.', description='Returns Tespa team SR and individual SR')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+'''
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await bot.say(content)
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))   
+'''    
+@bot.command(description='Enter link to Tespa team and recieve team analytics')
+async def tespa(team):
+    output = analyze(team)
+    output = '```autohotkey' +'\n'+ output + '```'
+    await bot.say(output)
+    
+@bot.command(description='Enter battletag and recieve sr for given battletag')
+async def sr(name):
+    output = findSR(name)
+    output = '```autohotkey' +'\n' + output + '```'
+    await bot.say(output)
+
+    
+bot.run('NDk0MzAxODMwMzgyMDkyMzIw.Doxi8g.KfJtft4C_OOo2Xwx0iBksFCu_-M')
---
+++ ranks.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 17:27:50 2018
+
+@author: ryans
+"""
+
+import requests
+from bs4 import BeautifulSoup
+def analyze(team):
+    #r = requests.get('https://compete.tespa.org/tournament/111/team/23056')
+    r = requests.get(team)
+    #print(r.text[0:500])
+    soup = BeautifulSoup(r.text, 'html.parser')
+    players = len(soup.find_all('td', attrs={'class':'compete-player-name'}))
+    #results = soup.find_all('table', attrs={'class':'compete-table'})
+    results = soup.find_all('td')
+    #print(results)
+    #print(len(results))
+    battletag = []
+    battletag2 = []
+    sr = []
+    avg = 0
+    count = 0
+    for player in range(1, players+1):
+        battletag.append(results[player*7 -5])
+        battletag2.append(str(battletag[player-1]).strip('/<td>'))
+    for player2 in battletag2:
+        #print(player2)
+        #search = 'https://www.overbuff.com/players/pc/' + player2.replace('#','-')
+        #search = 'https://playoverwatch.com/en-us/career/pc/' + player2.replace('#','-')
+        #print(search)                                                         
+        #r2 = requests.get('https://www.overbuff.com/players/pc/' + player2.replace('#','-'))    
+        r2 = requests.get('https://playoverwatch.com/en-us/career/pc/' + player2.replace('#','-'))
+        soup2 = BeautifulSoup(r2.text, 'html.parser')
+        #print('https://www.overbuff.com/players/pc/' + player2.replace('#','-'))
+        #print(r2.text[0:100])
+        #results2 = soup2.find_all('span', attrs={'class':'player-skill-rating'})
+        results2 = soup2.find('div', attrs={'class':'u-align-center h5'})
+        sr1 = str(results2)
+        sr1 = sr1.strip('<div class="u-align-center h5">')
+        sr1 = sr1.strip('/')
+        sr1 = sr1.strip('<')
+        #print(player2)
+        #print(sr1)
+        #print(results2)
+        if sr1 != 'No':
+            avg += int(sr1)
+            count += 1
+        playerandsr = str(player2 + ': ' + sr1 + '      ')
+        sr.append(playerandsr)
+    avg = round(avg/count)
+    sr.append('Average SR: ' + str(avg))
+    stringer = '\n'.join(map(str, sr))
+    return(stringer)
+
+def findSR(name):
+    newName = name.replace('#','-')
+    r = requests.get('https://playoverwatch.com/en-us/career/pc/' + newName)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    sr = soup.find('div', attrs={'class':'u-align-center h5'})
+    sr = str(sr)
+    sr = sr.strip('<div class="u-align-center h5">')
+    sr = sr.strip('/')
+    sr = sr.strip('<')
+    #for image in soup.find('div', attrs={'class':'competitive-rank'}):
+        #print(image.get('src'))
+        #images = str(image.get('src'))
+    #image = str(image)
+    #image = image.strip('imgsrc=')
+    #print(image)
+    outputter = str(name + ': ' + sr)
+    return(outputter)
+    
+def test():
+    array = [1,2,3,4]
+    string = '\n'.join(map(str, array))
+    return(string)
