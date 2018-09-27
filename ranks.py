364aba8ef235c7c64491e39badbff547b6284a1a
Cole Mundus <ColeMundus@users.noreply.github.com>
2018-09-26 22:21:46 -0500

    Update ranks.py

--- ranks.py
+++ ranks.py
@@ -2,9 +2,9 @@
 
 import requests
 from bs4 import BeautifulSoup
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
 
 def analyze(team):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
     s = BeautifulSoup(requests.get(team, headers=headers).content, 'html.parser')
     team_name = s.find(class_='hdg-em').text
     bnet = [t.find_next('td').text for t in s.find_all(class_='compete-player-name')]
@@ -22,3 +22,11 @@ def analyze(team):
         stringer += f'{player:<20} | {skill_rating:>4} | {", ".join(top_played)}\n'
     stringer += f'Average Team SR: {sum(average)//len(average)}\n'
     return stringer
+
+def findSR(name):
+    p = BeautifulSoup(requests.get(f"https://www.overbuff.com/players/pc/{'-'.join(name.split('#'))}?mode=competitive", headers=headers).content, 'html.parser')
+    try:
+        skill_rating = p.find(class_='player-skill-rating').text
+    except:
+        skill_rating = 'NA'
+    return f'{name}: {skill_rating}'
